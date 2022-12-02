9ea30c9d3627b1721ba3380ed83ac96b080960d2
Esteban Oller <estebanoller83@gmail.com>
2022-12-02 19:07:20 -0300

    Varias modificaciones

--- src/js/gotop.js
+++ src/js/gotop.js
@@ -1,8 +1,8 @@
 window.onscroll = function() {
     console.log(document.documentElement.scrollTop);
     if (document.documentElement.scrollTop > 300) { // Cuando sea mayor a 100 se muestre
-        document.querySelector('.go-top-container').classList.add('show');
+        document.querySelector('.button-WhatsApp-container').classList.add('show');
     } else { // Cuando sea menor a 100, remueve esa clase (es decir el boton)
-        document.querySelector('.go-top-container').classList.remove('show');
+        document.querySelector('.button-WhatsApp-container').classList.remove('show');
     }
 }
